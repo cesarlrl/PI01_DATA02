c0d7829e6396faf0155915c57f07fc9d93be7715
Cesar <cesarrosendo61@gmail.com>
2022-09-12 23:57:43 -0400

    corrections

--- cleaningdata.sql
+++ cleaningdata.sql
@@ -7,3 +7,19 @@ ALTER TABLE constructors DROP COLUMN Id;
 
 SELECT * FROM drivers;
 ALTER TABLE drivers DROP COLUMN Id;
+
+INSERT INTO lap_times SELECT *
+FROM (SELECT * FROM lap_times_split_1
+UNION
+SELECT * FROM lap_times_split_2
+UNION
+SELECT * FROM lap_times_split_3
+UNION
+SELECT * FROM lap_times_split_4
+UNION
+SELECT * FROM lap_times_split_5) a;
+
+INSERT INTO qualifying SELECT *
+FROM (SELECT * FROM qualifying_split_1
+UNION
+SELECT * FROM qualifying_split_2) a;
